8f9ad656bbe76c0611807a7776109b5c701d9014
David Felipe <79460638+D-Felipe@users.noreply.github.com>
2024-11-26 02:56:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -52,3 +52,9 @@ with open('emotions.txt', 'r') as file:
 print(emotion_list)
 w = Counter(emotion_list)
 print(w)
+
+fig, ax1 = plt.subplots()
+ax1.bar(w.keys(), w.values())
+fig.autofmt_xdate()
+plt.savefig('graph.png')
+plt.show()
